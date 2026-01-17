60ddbeee8659c25363e6bd5723a31acc70d99a11
Le  Toan <letoan@MacBookPro.lan>
2026-01-17 18:07:54 +0700

    Upgrade flutter 3.38.7

--- packages/syncfusion_flutter_calendar/lib/src/calendar/appointment_layout/allday_appointment_layout.dart
+++ packages/syncfusion_flutter_calendar/lib/src/calendar/appointment_layout/allday_appointment_layout.dart
@@ -6,6 +6,7 @@ import 'package:syncfusion_flutter_core/theme.dart';
 import '../../../calendar.dart';
 import '../appointment_engine/appointment_helper.dart';
 import '../common/calendar_view_helper.dart';
+import '../common/calendar_view_helper.dart' as calendarHelper;
 import '../common/date_time_engine.dart';
 
 /// Used to holds the all day appointment views in calendar widgets.
@@ -48,7 +49,7 @@ class AllDayAppointmentLayout extends StatefulWidget {
 
   /// Holds the selection details and user to trigger repaint to draw the
   /// selection.
-  final ValueNotifier<SelectionDetails?> repaintNotifier;
+  final ValueNotifier<calendarHelper.SelectionDetails?> repaintNotifier;
 
   /// Used to get the calendar state details.
   final UpdateCalendarState updateCalendarState;
@@ -449,7 +450,7 @@ class _AllDayAppointmentRenderWidget extends MultiChildRenderObjectWidget {
   final CalendarView view;
   final List<DateTime> visibleDates;
   final List<CalendarAppointment>? visibleAppointments;
-  final ValueNotifier<SelectionDetails?> repaintNotifier;
+  final ValueNotifier<calendarHelper.SelectionDetails?> repaintNotifier;
   final double timeLabelWidth;
   final double allDayPainterHeight;
   final bool isRTL;
@@ -766,11 +767,11 @@ class _AllDayAppointmentRenderObject extends CustomCalendarRenderObject {
     _allDayHoverPosition.addListener(markNeedsPaint);
   }
 
-  ValueNotifier<SelectionDetails?> _selectionNotifier;
+  ValueNotifier<calendarHelper.SelectionDetails?> _selectionNotifier;
 
-  ValueNotifier<SelectionDetails?> get selectionNotifier => _selectionNotifier;
+  ValueNotifier<calendarHelper.SelectionDetails?> get selectionNotifier => _selectionNotifier;
 
-  set selectionNotifier(ValueNotifier<SelectionDetails?> value) {
+  set selectionNotifier(ValueNotifier<calendarHelper.SelectionDetails?> value) {
     if (_selectionNotifier == value) {
       return;
     }
@@ -1436,7 +1437,7 @@ class _AllDayAppointmentRenderObject extends CustomCalendarRenderObject {
   /// Used to pass the argument of create box painter and it is called when
   /// decoration have asynchronous data like image.
   void _updateSelectionDecorationPainter() {
-    selectionNotifier.value = SelectionDetails(
+    selectionNotifier.value = calendarHelper.SelectionDetails(
         selectionNotifier.value!.appointmentView,
         selectionNotifier.value!.selectedDate);
   }
--- packages/syncfusion_flutter_calendar/lib/src/calendar/views/calendar_view.dart
+++ packages/syncfusion_flutter_calendar/lib/src/calendar/views/calendar_view.dart
@@ -18,6 +18,7 @@ import '../appointment_engine/recurrence_helper.dart';
 import '../appointment_layout/allday_appointment_layout.dart';
 import '../appointment_layout/appointment_layout.dart';
 import '../common/calendar_view_helper.dart';
+import '../common/calendar_view_helper.dart' as calendarHelper;
 import '../common/date_time_engine.dart';
 import '../views/day_view.dart';
 import '../views/month_view.dart';
@@ -5634,8 +5635,8 @@ class _CalendarViewState extends State<_CalendarView>
   late double _timeIntervalHeight;
   final UpdateCalendarStateDetails _updateCalendarStateDetails =
       UpdateCalendarStateDetails();
-  ValueNotifier<SelectionDetails?> _allDaySelectionNotifier =
-      ValueNotifier<SelectionDetails?>(null);
+  ValueNotifier<calendarHelper.SelectionDetails?> _allDaySelectionNotifier =
+  ValueNotifier<calendarHelper.SelectionDetails?>(null);
   late ValueNotifier<Offset?> _viewHeaderNotifier;
   final ValueNotifier<Offset?> _calendarCellNotifier =
           ValueNotifier<Offset?>(null),
@@ -5803,7 +5804,7 @@ class _CalendarViewState extends State<_CalendarView>
     /// select the same month cell and move to day view then the view show
     /// calendar cell selection and all day panel selection.
     if (oldWidget.view != widget.view) {
-      _allDaySelectionNotifier = ValueNotifier<SelectionDetails?>(null);
+      _allDaySelectionNotifier = ValueNotifier<calendarHelper.SelectionDetails?>(null);
       final DateTime today = DateTime.now();
       _currentTimeNotifier = ValueNotifier<int>(
           (today.day * 24 * 60) + (today.hour * 60) + today.minute);
@@ -9401,7 +9402,7 @@ class _CalendarViewState extends State<_CalendarView>
       return;
     }
 
-    _allDaySelectionNotifier.value = SelectionDetails(view, date);
+    _allDaySelectionNotifier.value = calendarHelper.SelectionDetails(view, date);
   }
 
   //// Handles the onTap callback for day view cells, all day panel, and view
